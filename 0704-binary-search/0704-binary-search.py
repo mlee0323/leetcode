6f1966031a3a816c2e8fe9a8f28da16712ef008a
Minseok Lee <musicbox0323@gmail.com>
2025-11-14 15:00:15 +0900

    Time: 0 ms (100%), Space: 18.9 MB (5.98%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,13 +1,16 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        numMap = {}
-        n = len(nums)
+        left = 0
+        right = len(nums) - 1
 
-        for i in range(n):
-            numMap[nums[i]] = i
-        
-        for i in range(n):
-            if target in numMap:
-                return numMap[target]
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                return (-1)
+                right = mid - 1
+            
+        return -1
